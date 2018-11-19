7fe5f558939bd310adab9fd89e0938544a87095b
Jacob Levin <jhlevin@ilstu.edu>
2018-11-18 18:47:54 -0600

    Added comments

--- TCPClient.java
+++ TCPClient.java
@@ -6,14 +6,16 @@ class TCPClient {
 	//Establishing the socket on the given server
 	Socket clientSocket = new Socket("192.168.2.30", 12375);
 	
+	//Creating an instance of the file transfer helper class
 	FileTransferProcessor fileTransfer = new FileTransferProcessor(clientSocket);
+
 	
-	
-	
-	
+	//Creates new test file entitled test.PNG
 	File file = new File("test.PNG");
 	
+	//Calling the sendFile method from FileTransferProcess class
 	fileTransfer.sendFile(file);
+	
 	
 	clientSocket.close();	
 		
--- TCPServer.java
+++ TCPServer.java
@@ -3,19 +3,16 @@ import java.net.*;
 
 class TCPServer{
 	public static void main(String argv[]) throws Exception{
-		
-		
-		
 		//Establish ServerSocket on port 12375
 		ServerSocket welcomeSocket = new ServerSocket (12375);
 
 		//Establish socket connection
 		Socket socket = welcomeSocket.accept();
 		
+		//Creating an instance of the file transfer helper class
 		FileTransferProcessor fileTransfer = new FileTransferProcessor(socket);
 		
-		
-
+		//Attempts to receive a test file called test.PNG
 		fileTransfer.receiveFile("test.PNG");
 	
 	}
