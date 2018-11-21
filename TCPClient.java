53154ccf37fdc6c6d9c1e8e45a486c8ca257391b
Jacob Levin <jhlevin@ilstu.edu>
2018-11-21 12:59:31 -0600

    Changed framework

--- TCPClient.java
+++ TCPClient.java
@@ -2,23 +2,40 @@ import java.io.*;
 import java.net.*;
 
 class TCPClient {
-    public static void main(String argv[]) throws Exception{
-	//Establishing the socket on the given server
-	Socket clientSocket = new Socket("192.168.2.30", 12375);
-	
-	//Creating an instance of the file transfer helper class
-	FileTransferProcessor fileTransfer = new FileTransferProcessor(clientSocket);
+	private Socket clientSocket = null;
+	private DataInputStream is = null;
+	private DataOutputStream out = null;
 
-	
-	//Creates new test file entitled test.PNG
-	File file = new File("test.PNG");
-	
-	//Calling the sendFile method from FileTransferProcess class
-	fileTransfer.sendFile(file);
-	
-	
-	clientSocket.close();	
-		
+
+	public TCPClient(){
+		try {
+			//Establishing the socket on the given server
+			clientSocket = new Socket("192.168.2.30", 12375);
+			System.out.println("Connected");
+
+
+
+			//Creating an instance of the file transfer helper class
+			//FileTransferProcessor fileTransfer = new FileTransferProcessor(clientSocket);
+
+
+			//Creates new test file entitled test.PNG
+			//File file = new File("test.PNG");
+
+			//Calling the sendFile method from FileTransferProcess class
+			//fileTransfer.sendFile(file);
+
+
+			clientSocket.close();
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+
+    public static void main(String argv[]) throws Exception{
+		TCPClient client = new TCPClient();
+		System.out.println("Please choose from the following:\n1: Download a file\n2: upload a file");
     }
 	
 }
--- TCPServer.java
+++ TCPServer.java
@@ -1,20 +1,57 @@
-import java.io.*;
+import java.io.File;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.*;
+import java.util.ArrayList;
 
-class TCPServer{
+class TCPServer {
+	private ServerSocket welcomeSocket = null;
+	private Socket socket = null;
+	private ObjectInputStream objectInputStream= null;
+	private ObjectOutputStream objectOutputStream= null;
+	private static ArrayList<String> fileNames = new ArrayList();
+
+
+	public TCPServer() {
+		try {
+			//Establish ServerSocket on port 12375
+			welcomeSocket = new ServerSocket(12375);
+			System.out.println("Server Started\nWaiting for Client...");
+
+			//Establish socket connection
+			socket = welcomeSocket.accept();
+			objectInputStream = new ObjectInputStream(socket.getInputStream());
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+
+
+
+			//Creating an instance of the file transfer helper class
+			//FileTransferProcessor fileTransfer = new FileTransferProcessor(socket);
+
+			//Attempts to receive a test file called test.PNG
+			//fileTransfer.receiveFile("test.PNG");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public static ArrayList<String> getFileNames(String curDirectory){
+		File[] files = new File(curDirectory).listFiles();
+		//If this pathname does not denote a directory, then listFiles() returns null.
+
+			for(File file :files) {
+				if (file.isFile()) {
+					fileNames.add(file.getName());
+				}
+			} return (fileNames);
+	}
+
+
 	public static void main(String argv[]) throws Exception{
-		//Establish ServerSocket on port 12375
-		ServerSocket welcomeSocket = new ServerSocket (12375);
-
-		//Establish socket connection
-		Socket socket = welcomeSocket.accept();
-		
-		//Creating an instance of the file transfer helper class
-		FileTransferProcessor fileTransfer = new FileTransferProcessor(socket);
-		
-		//Attempts to receive a test file called test.PNG
-		fileTransfer.receiveFile("test.PNG");
-	
+		//TCPServer server = new TCPServer();
+		System.out.println(getFileNames("C:\\DB"));
 	}
 
   }
