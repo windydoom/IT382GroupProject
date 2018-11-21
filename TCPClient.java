61b50cbbd06c0c471813d35823050a4c066751a4
Jacob Levin <jhlevin@ilstu.edu>
2018-11-21 16:27:50 -0600

    basic text ui started and able to download from server to client without specified location

--- TCPClient.java
+++ TCPClient.java
@@ -1,24 +1,73 @@
 import java.io.*;
 import java.net.*;
+import java.util.Scanner;
 
 class TCPClient {
+	private int bufferSize;
 	private Socket clientSocket = null;
+	private InputStream inputStream = null;
+	private BufferedOutputStream bufferedOutputStream = null;
+	private FileOutputStream fileOutputStream = null;
 	private ObjectInputStream objectInputStream= null;
 	private ObjectOutputStream objectOutputStream= null;
+	private DataInputStream  dataInputStream   = null;
 	private ByteArrayInputStream byteArrayInputStream= null;
 	private BufferedReader bufferedReader = null;
 	private DataOutputStream dataOutputStream = null;
+	private FileTransferProcessor fileTransferProcessor = null;
+	private Scanner console = null;
 
 	public TCPClient(){
 		try {
+			String uploadOrDownload = "";
+			String fileName = "";
+
+			console = new Scanner(System.in);
 			//Establishing the socket on the given server
-			clientSocket = new Socket("localhost", 12375);
+			clientSocket = new Socket("localhost", 12376);
 			System.out.println("Connected");
 
+			System.out.println("Please choose from the following:\n1: Download a file\n2: upload a file");
+
+			//Getting input and output streams
 			bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 			dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-			System.out.println(bufferedReader.readLine());
+			//Client's choice to upload or download
+			uploadOrDownload = console.next();
+
+			//Writing whether the client wants to download or upload
+			dataOutputStream.writeBytes(uploadOrDownload + "\n");
+
+			//Creates a new FileTransferProcessor to actually handle uploading or download a file
+			fileTransferProcessor = new FileTransferProcessor(clientSocket);
+
+
+			switch (Integer.parseInt(uploadOrDownload)){
+
+				//Download
+				case 1:
+					System.out.println("Choose from the following files to download: " + bufferedReader.readLine());
+					fileName = console.next();
+
+					//Writing whether the client wants to download or upload
+					dataOutputStream.writeBytes(fileName + "\n");
+
+					System.out.println("Made it this far");
+
+					fileTransferProcessor = new FileTransferProcessor(clientSocket);
+					fileTransferProcessor.receiveFile(fileName);
+
+					break;
+
+				case 2:
+					System.out.println("2");
+					break;
+
+			}
+
+
+
 
 			//Creating an instance of the file transfer helper class
 			//FileTransferProcessor fileTransfer = new FileTransferProcessor(clientSocket);
@@ -40,7 +89,6 @@ class TCPClient {
 
     public static void main(String argv[]) throws Exception{
 		TCPClient client = new TCPClient();
-		//System.out.println("Please choose from the following:\n1: Download a file\n2: upload a file");
     }
 	
 }
--- TCPServer.java
+++ TCPServer.java
@@ -3,30 +3,72 @@ import java.net.*;
 import java.util.ArrayList;
 
 class TCPServer {
+	private byte[] buffer = null;
 	private ServerSocket welcomeSocket = null;
 	private Socket socket = null;
+	private FileInputStream fileInputStream = null;
+	private BufferedInputStream bufferedInputStream = null;
+	private BufferedOutputStream bufferedOutputStream = null;
 	private ObjectInputStream objectInputStream= null;
 	private ObjectOutputStream objectOutputStream= null;
+	private DataInputStream dataInputStream = null;
 	private ByteArrayOutputStream byteArrayOutputStream= null;
 	private DataOutputStream dataOutputStream = null;
 	private BufferedReader bufferedReader = null;
+	private FileTransferProcessor fileTransferProcessor= null;
 	private static ArrayList<String> fileNames = new ArrayList();
 
 
 	public TCPServer() {
 		try {
 			//Establish ServerSocket on port 12375
-			welcomeSocket = new ServerSocket(12375);
+			welcomeSocket = new ServerSocket(12376);
 			System.out.println("Server Started\nWaiting for Client...");
 
 			//Establish socket connection
 			socket = welcomeSocket.accept();
 
+			//Getting input and output streams
 			dataOutputStream = new DataOutputStream(socket.getOutputStream());
 			bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			dataOutputStream.writeBytes(getFileNames());
-			dataOutputStream.writeBytes("\n");
+
+			//Creates a new FileTransferProcessor to actually handle uploading or download a file
+			fileTransferProcessor = new FileTransferProcessor(socket);
+
+
+			//Will receive the client's request to either upload or download a file via String then will be switched
+			switch(Integer.parseInt(bufferedReader.readLine())){
+
+				//Download a file
+				case 1:
+					//Writing the file names available for download
+					dataOutputStream.writeBytes(getFileNames());
+					dataOutputStream.writeBytes("\n");
+
+					System.out.println("before File");
+
+					File file = new File(bufferedReader.readLine());
+
+					System.out.println(file.toString());
+
+					fileTransferProcessor = new FileTransferProcessor(socket);
+					fileTransferProcessor.sendFile(file);
+
+
+
+
+					break;
+				//Upload a file
+				case 2:
+					System.out.println("case 2");
+					break;
+			}
+
+
+
+
+
 
 
 			//Creating an instance of the file transfer helper class
@@ -54,7 +96,6 @@ class TCPServer {
 
 	public static void main(String argv[]) throws Exception{
 		TCPServer server = new TCPServer();
-		//System.out.println(getFileNames("C:\\DB"));
 	}
 
   }
